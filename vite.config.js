c076b9992efe6077df6a6c1499482d867b6750ca
karthik-07 <karthikvs216@gmail.com>
2023-12-20 15:25:14 -0500

    Added Hashrouter instead of BrowserRouter

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-database/'
+  base: '/movie-database/',
+  build: {
+    assetsDir: 'src/assets/'
+  }
+
 })
+
